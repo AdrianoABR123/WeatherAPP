48c9b05004add14b3d9ef8c449899990edc38d0b
AdrianoABR123 <drianoeloy@gmail.com>
2026-04-25 15:39:05 -0300

    ref: Modifying the components used in the text input.

--- app/src/main/java/com/example/weatherapp/ui/components/PasswordField.kt
+++ app/src/main/java/com/example/weatherapp/ui/components/PasswordField.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
@@ -27,51 +29,16 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun PasswordInput(
-    state: TextFieldState
-    ,label: String) {
-
-    var showPassword by remember { mutableStateOf(false) }
-
-    BasicSecureTextField(
-        state = state,
-        textObfuscationMode = if (showPassword) TextObfuscationMode.Visible else TextObfuscationMode.Hidden,
-        modifier = Modifier.fillMaxWidth(0.9f).padding(top = 12.dp),
-        decorator = { innerTextField ->
-            OutlinedTextFieldDefaults.DecorationBox(
-                value = state.text.toString(),
-                innerTextField = innerTextField,
-                enabled = true,
-                singleLine = true,
-                visualTransformation = VisualTransformation.None,
-                interactionSource = remember { MutableInteractionSource() },
-                label = { Text(label) },
-                placeholder = { Text("Digite Aqui...") },
-                trailingIcon = {
-                    IconButton(onClick = { showPassword = !showPassword }) {
-                        Icon(
-                            imageVector = if (showPassword) {
-                                Icons.Filled.Visibility
-                            } else {
-                                Icons.Filled.VisibilityOff
-                            },
-                            contentDescription = if (showPassword) {
-                                "Esconder Senha"
-                            } else {
-                                "Mostrar Senha"
-                            }
-                        )
-                    }
-                },
-                container = {
-                    OutlinedTextFieldDefaults.Container(
-                        enabled = true,
-                        isError = false,
-                        interactionSource = remember { MutableInteractionSource() },
-                        colors = OutlinedTextFieldDefaults.colors(),
-                        shape = OutlinedTextFieldDefaults.shape
-                    )
-                }
-            )
-        }
+    state: String,
+    label: String,
+    modifier: Modifier,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = state,
+        label = { Text(text = label) },
+        modifier = modifier.fillMaxWidth(0.9f),
+        onValueChange = onValueChange,
+        visualTransformation = PasswordVisualTransformation()
     )
 }
