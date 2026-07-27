b043c39fb81eaca31555862efd267a7881ac2435
AdrianoABR123 <drianoeloy@gmail.com>
2026-07-26 23:53:49 -0300

    Feat: Mostrando as condições climáticas atuais na lista de ciadades

--- app/src/main/java/com/example/weatherapp/ui/pages/ListPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/pages/ListPage.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,6 +30,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.api.WeatherService
+import com.example.weatherapp.api.toWeather
 import com.example.weatherapp.db.fb.FBCity
 import com.example.weatherapp.db.fb.FBDatabase
 import com.example.weatherapp.db.fb.FBUser
@@ -36,19 +38,18 @@ import com.example.weatherapp.model.City
 import com.example.weatherapp.model.User
 import com.google.android.gms.maps.model.LatLng
 import com.example.weatherapp.db.fb.toFBTCity
+import com.example.weatherapp.model.Weather
 
-
-private fun getCities() = List(20 ) { i ->
-    City(name = "Cidade $i", weather = "Carregando clima ...")
-}
 
 @Composable
 fun CityItem(
     city: City,
+    weather: Weather,
     onClick: () -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    val desc = if (weather == Weather.LOADING) "Carregando clima..." else weather.desc
     Row(
         modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
@@ -63,7 +64,7 @@ fun CityItem(
                     text = city.name,
                     fontSize = 24.sp)
                 Text(modifier = Modifier,
-                    text = city.weather?:"Carregando clima...",
+                    text = desc,
                     fontSize = 16.sp)
             }
             IconButton(onClick = onClose) {
@@ -73,9 +74,11 @@ fun CityItem(
 }
 
 class MainViewModel (private val db: FBDatabase, private val service : WeatherService) : ViewModel(), FBDatabase.Listener {
-    private val _cities = mutableStateListOf<City>()
-    val cities
-        get() = _cities.toList()
+    private val _cities = mutableStateMapOf<String, City>()
+
+    val cities : List<City>
+        get() = _cities.values.toList().sortedBy { it.name }
+    private val _weather = mutableStateMapOf<String, Weather>()
     private val _user = mutableStateOf<User?>(null)
     val user: User?
         get() = _user.value
@@ -117,22 +120,31 @@ class MainViewModel (private val db: FBDatabase, private val service : WeatherSe
     }
 
     override fun onCityAdded(city: FBCity) {
-        _cities.add(city.toCity())
+        _cities[city.name!!] = city.toCity()
     }
 
     override fun onCityUpdated(city: FBCity) {
-        val updatedCity = city.toCity()
-        val index = _cities.indexOfFirst { it.name == updatedCity.name }
-        if (index != -1) {
-            _cities[index] = updatedCity
-        } else {
-            _cities.add(updatedCity)
-        }
+        _cities.remove(city.name)
+        _cities[city.name!!] = city.toCity()
     }
 
     override fun onCityRemoved(city: FBCity) {
-        _cities.remove(city.toCity())
+        _cities.remove(city.name)
     }
+
+    private fun loadWeather(name: String) {
+        service.getWeather(name) { apiWeather ->
+            apiWeather?.let {
+                _weather[name] = apiWeather.toWeather()
+            }
+        }
+    }
+
+    fun weather (name: String) = _weather.getOrPut(name) {
+        loadWeather(name)
+        Weather.LOADING // retorno
+    }
+
 }
 
 class MainViewModelFactory(private val db : FBDatabase, private val service : WeatherService) :
@@ -151,28 +163,37 @@ fun ListPage(
     viewModel: MainViewModel
 ) {
     val cityList = viewModel.cities
-    val context = LocalContext.current
+    val context = LocalContext.current as? Activity
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        items(cityList, key = { it.name }) { city ->
-            CityItem(city = city, onClose = {
-                Toast.makeText(
-                    context,
-                    "Cidade Excluida!",
-                    Toast.LENGTH_LONG
-                ).show()
-                
-                viewModel.remove(city)
-            }, onClick = {
-                Toast.makeText(
-                    context,
-                    "Cidade Clicada!",
-                    Toast.LENGTH_LONG
-                ).show()
-            })
+        items(
+            items = cityList,
+            key = {it.name}
+        ) {
+            city ->
+            CityItem(
+                city = city,
+                weather = viewModel.weather(city.name),
+                onClose = {
+                    Toast.makeText(
+                        context,
+                        "Cidade Excluida!",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    viewModel.remove(city)
+                },
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "Cidade Clicada!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            )
         }
     }
 }
