d3e565efe95aff173966d81427019b7ea8d10120
AdrianoABR123 <drianoeloy@gmail.com>
2026-08-07 19:00:21 -0300

    passo 1 - Prática 10

--- app/src/main/java/com/example/weatherapp/ui/pages/ListPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/pages/ListPage.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -55,6 +57,8 @@ fun CityItem(
     modifier: Modifier = Modifier
 ){
     val desc = if (weather == Weather.LOADING) "Carregando clima..." else weather.desc
+    val icon = if (city.isMonitored) Icons.Filled.Notifications else Icons.Outlined.Notifications
+
     Row(
         modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
@@ -68,9 +72,18 @@ fun CityItem(
 
         Spacer(modifier = Modifier.size(12.dp))
             Column(modifier = modifier.weight(1f)) {
-                Text(modifier = Modifier,
-                    text = city.name,
-                    fontSize = 24.sp)
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = city.name,
+                        fontSize = 24.sp
+                    )
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = "Monitorada?",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
                 Text(modifier = Modifier,
                     text = desc,
                     fontSize = 16.sp)
@@ -188,7 +201,13 @@ class MainViewModel (private val db: FBDatabase, private val service : WeatherSe
         }
     }
 
+    fun updateCity(city: City) {
+        db.update(city.toFBTCity());
+    }
 
+    fun update(city: City) {
+        updateCity(city)
+    }
 
 }
 
